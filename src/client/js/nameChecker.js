6c86dba2719ded40ddccffe284f547de8616f724
Marwan Samamreh <119597263+MarwanSamamreh@users.noreply.github.com>
2024-08-24 13:31:26 +0300

    Update nameChecker.js

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,34 +1,7 @@
-import { checkForURL } from "../src/client/js/nameChecker";
+// src/client/js/nameChecker.js
 
-describe("Testing the checkForURL function", () => {
-  test("should return true for valid URLs", () => {
-    const validURLs = [
-      "http://example.com",
-      "https://example.com",
-      "http://www.example.com",
-      "https://www.example.com",
-      "http://example.com/path/to/resource",
-      "https://example.com/path/to/resource",
-      "http://example.com:8080",
-    ];
-
-    validURLs.forEach((url) => {
-      expect(checkForURL(url)).toBe(true);
-    });
-  });
-
-  test("should return false for invalid URLs", () => {
-    const invalidURLs = [
-      "example.com",
-      "http://",
-      "https://",
-      "ftp://example.com",
-      "http:// example.com",
-      "http://example.com:abc",
-    ];
-
-    invalidURLs.forEach((url) => {
-      expect(checkForURL(url)).toBe(false);
-    });
-  });
-});
+export function checkForURL(inputURL) {
+  // Regular expression for validating URLs
+  const urlRegex = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,./?%&=]*)?$/i;
+  return urlRegex.test(inputURL);
+}
